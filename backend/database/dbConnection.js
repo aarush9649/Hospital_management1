c57eac6112ffdb709b81c1d4234fc74e44e3ac89
Avanish <avanishkumar4400@gmail.com>
2025-07-28 18:03:58 +0530

    add new

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -1,16 +1,12 @@
 import mongoose from "mongoose";
 
-export const dbConnection = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      dbName: "MERN_STACK_HOSPITAL_MANAGEMENT_SYSTEM_DEPLOYED",
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+export const dbConnection = () => {
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log("✅ Connected to MongoDB Atlas database!");
+    })
+    .catch((err) => {
+      console.error("❌ Database connection error:", err);
     });
-
-    console.log(`✅ MongoDB connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error("❌ Error connecting to MongoDB:", error.message);
-    process.exit(1); // Exit with failure
-  }
 };
